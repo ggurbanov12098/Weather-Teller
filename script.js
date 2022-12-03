a91551637fb063fda5d854aa97530ab04eb33a61
ggurbanov12098 <qabil.qurbanov007@gmail.com>
2022-12-03 14:05:44 +0400

    v1.0 (no detailed info)

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let weather = {
-    "apiKey": "50697639d130e6a0f1a08e4c6825c5f0",
+    apiKey: "50697639d130e6a0f1a08e4c6825c5f0",
     fetchWeather: function(city){
         fetch("https://api.openweathermap.org/data/2.5/weather?q=" 
         + city 
@@ -13,13 +13,13 @@ let weather = {
         const { icon, description } = data.weather[0];
         const { temp, humidity } = data.main;
         const { speed } = data.wind;
-        console.log(name, icon, description, temp, humidity, speed)
         document.querySelector(".city").innerHTML = "Weather in " + name;
         document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png"
         document.querySelector(".description").innerText = description;
         document.querySelector(".temp").innerText = temp + "°C";
         document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
         document.querySelector(".wind").innerText = "Wind speed: " + speed + "km/h";
+        document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + "')"
     },
     search: function(){
         this.fetchWeather(document.querySelector(".search-bar").value);
@@ -29,3 +29,11 @@ let weather = {
 document.querySelector(".search-button").addEventListener("click", function(){
     weather.search();
 });
+
+document.querySelector(".search-bar").addEventListener("keyup", function(event){
+    if(event.key == "Enter"){
+        weather.search();
+    }
+})
+
+// weather.fetchWeather("Baku");
